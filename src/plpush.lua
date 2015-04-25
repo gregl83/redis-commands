6425b228b1d069651b602d610ea42f737d0ec980
Gregory Langlais <general@gregorylanglais.com>
2015-04-25 18:57:55 -0400

    lpush with ARGV passed directly to lpush
    - need to test ARGV syntax

--- src/plpush.lua
+++ src/plpush.lua
@@ -1,13 +1,5 @@
-local result
-local next = next
 local rcall = redis.call
 
-local member = rcall('zrange', KEYS[1], 0, 0, 'WITHSCORES')
-result = member
+local key = KEYS[1] .. ':' .. KEYS[2]
 
-if nil ~= next(member) then
-    rcall('zadd', KEYS[2], member[2], member[1])
-    rcall('zremrangebyrank', KEYS[1], 0, 0)
-end
-
-return result
+return rcall('lpush', key, ARGV)
