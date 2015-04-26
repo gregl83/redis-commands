737a6eda49cf6cfb83764aeef8c7bec9b9bad509
Gregory Langlais <general@gregorylanglais.com>
2015-04-25 21:27:17 -0400

    prpoplpush tested

--- src/prpoplpush.lua
+++ src/prpoplpush.lua
@@ -1,9 +1,10 @@
 local result
+local pairs = pairs
 local rcall = redis.call
 local priorityLists = {'critical', 'high', 'medium', 'low'}
 
 for key, val in pairs(priorityLists) do
-    if nil == result then
+    if not result then
         local source = KEYS[1] .. ':' .. val
         local destination = KEYS[2] .. ':' .. val
         result = rcall('rpoplpush', source, destination)
--- test/fixtures/plist.lua
+++ test/fixtures/plist.lua
@@ -10,30 +10,30 @@ local argv2 = tonumber(ARGV[2])
 local argv3 = tonumber(ARGV[3])
 local argv4 = tonumber(ARGV[4])
 
-key = KEY[1] .. ':critical'
+key = KEYS[1] .. ':critical'
 for member = 1, argv1, 1 do
-    rcall('rpush', key, member)
+    rcall('lpush', key, member)
 end
 insert(results, key)
 insert(results, argv1)
 
-key = KEY[1] .. ':high'
+key = KEYS[1] .. ':high'
 for member = 1, argv2, 1 do
-    rcall('rpush', key, member)
+    rcall('lpush', key, member)
 end
 insert(results, key)
 insert(results, argv2)
 
-key = KEY[1] .. ':medium'
+key = KEYS[1] .. ':medium'
 for member = 1, argv3, 1 do
-    rcall('rpush', key, member)
+    rcall('lpush', key, member)
 end
 insert(results, key)
 insert(results, argv3)
 
-key = KEY[1] .. ':low'
+key = KEYS[1] .. ':low'
 for member = 1, argv4, 1 do
-    rcall('rpush', key, member)
+    rcall('lpush', key, member)
 end
 insert(results, key)
 insert(results, argv4)
