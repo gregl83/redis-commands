725e3fa8d27181c1d555200978a7f944fb4abb5e
Gregory Langlais <general@gregorylanglais.com>
2015-06-10 14:27:31 -0400

    patched zrangeincrby withscores option

--- src/zrangeincrby.lua
+++ src/zrangeincrby.lua
@@ -9,7 +9,7 @@ if ARGV[3] ~= nil or ARGV[3] == 0 then
     while members[i] do
         insert(result, members[i])
         local score = rcall('zincrby', KEYS[1], ARGV[3], members[i])
-        if "WITHSCORES" == ARGV[4] then
+        if 'WITHSCORES' == ARGV[4] then
             insert(result, score)
         end
         i = i + 1
