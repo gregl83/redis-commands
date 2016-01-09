e1c9130ca303f64aee61fe7480109b90c823e69d
Gregory Langlais <general@gregorylanglais.com>
2016-01-09 12:39:42 -0500

    update hgetrand to functional state

---
+++ src/hgetrand.lua
@@ -0,0 +1,13 @@
+local result
+local rcall = redis.call
+
+math.randomseed(tonumber(tostring({}):match("0x(.*)$"),16))
+
+local fields = rcall('hkeys', KEYS[1])
+
+if nil ~= next(fields) then
+    local field = fields[math.random(#fields)]
+    result = rcall('hget', KEYS[1], field)
+end
+
+return result
